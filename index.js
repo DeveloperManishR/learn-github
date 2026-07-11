fc3db97097ef530ebbadaaccfcb91e528407eb7e
DeveloperManishR <manishrawat0711@gmail.com>
2026-07-12 02:35:59 +0530

    fucntion added

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(num1,num2){
+    if(num1<num2){
+        return 'Num should be grater '
+    }
+ return num1+num2
+}
+
+function sub(num1,num2){
+ return num1-num2
+}
+
+function mutiply(num1,num2){
+ return num1*num2
+}
+
+add(2,3)
+sub(3,4)
+mutiply(32,41)
